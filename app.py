c2b2c7fe037b3e12d6b7d467c5dca45a07f68444
Tsukalos <pedrolamkowski@gmail.com>
2024-10-22 19:36:59 -0300

    - updated app.py to only contain streamlit front - generation.py file has the api logic - updated db

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import streamlit as st
-
-from getpass import getpass
-import os
-
-from langchain.text_splitter import MarkdownTextSplitter
-from langchain_chroma import Chroma
-
-
-from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
-from langchain_google_genai import ChatGoogleGenerativeAI, HarmBlockThreshold, HarmCategory
-
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.prompts import PromptTemplate
 
 from argparse import ArgumentParser
-from loaders import book_loader, get_grimoire_docs
-
-from pathlib import Path
-
+from generation import T20Gen
+import streamlit as st
 
 parser = ArgumentParser("Ajudante do Mestre - Atlas de Arton")
 parser.add_argument("--generate_embeddings", action="store_true",
@@ -29,105 +12,15 @@ parser.add_argument("--generate_embeddings", action="store_true",
 parser.add_argument("--reveal_retrieved_docs", action="store_true")
 args = parser.parse_args()
 
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = getpass("Provide your Google API key here: ")
-
-safety_settings = {
-    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT : HarmBlockThreshold.BLOCK_NONE,
-    HarmCategory.HARM_CATEGORY_HATE_SPEECH : HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT : HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    HarmCategory.HARM_CATEGORY_HARASSMENT : HarmBlockThreshold.BLOCK_NONE,
-}
-
 st.set_page_config(page_title='AjudanTe20', page_icon='img/128px-D20_icon.png')
+
 @st.cache_resource
-def initialize_data():
-    
+def init_data():
+    textgen = T20Gen(args.generate_embeddings, args.reveal_retrieved_docs)
+    textgen.initialize_data()
+    return textgen
 
-    DATA_DIR = Path("./data/")
-
-    # define o modelo para gerar os embeddings
-    embedding_function = GoogleGenerativeAIEmbeddings(
-        model="models/text-embedding-004")
-    # aonde salvar os embeddings
-    chroma_dir = DATA_DIR / "chroma_db/"
-    if args.generate_embeddings:
-        # with open(os.path.join(DATA_DIR, 'atlas/output_clean.md'), 'r') as file:
-        #     md_text = file.read()
-
-        md_text = (DATA_DIR / 'atlas/output_clean.md').read_text()
-
-        docs = []
-        # define o divisor de arquivos para o .md do atlas de arton
-        splitter = MarkdownTextSplitter(chunk_size=4096, chunk_overlap=256)
-
-        # seções do livro
-        texts, metadatas = book_loader()
-        # divide os documentos dado o divisor.
-        docs += splitter.create_documents([md_text] + texts, metadatas=[
-                                          {"source": "atlas/output_clean.md"}] + metadatas)
-        docs += get_grimoire_docs()
-
-        print(f'Num. docs: {len(docs)}')
-
-        vectorstore = Chroma.from_documents(
-            collection_name="t20",
-            documents=docs, 
-            embedding=embedding_function, 
-            persist_directory=str(chroma_dir)
-        )
-    else:
-        vectorstore = Chroma(
-            collection_name="t20",
-            persist_directory=str(chroma_dir),
-            embedding_function=embedding_function)
-    
-    retriever = vectorstore.as_retriever(
-        search_type="mmr", 
-        search_kwargs={"k": 3}
-    )
-
-    prompt_file = Path(DATA_DIR) / 'prompt' / 't20.md'
-    template = (prompt_file).read_text()
-    custom_rag_prompt = PromptTemplate.from_template(template)
-
-    return retriever, custom_rag_prompt
-
-
-retriever, custom_rag_prompt = initialize_data()
-
-
-def format_docs(docs):
-    if args.reveal_retrieved_docs:
-        for doc in docs:
-            print(f'{doc}\n\n\n')
-    return "\n\n".join(doc.page_content for doc in docs)
-    
-def generate_response(text):
-    if text != '':
-        with st.spinner('Gerando...'):
-
-            llm = ChatGoogleGenerativeAI(
-                model=st.session_state.model_name,
-                temperature=st.session_state.temperature, 
-                max_tokens=8192, 
-                max_output_tokens=8192, 
-                max_retries=12, 
-                timeout=30,
-                safety_settings=safety_settings)
-            
-
-            rag_chain = (
-                {"context": retriever | format_docs,
-                    "question": RunnablePassthrough()}
-                | custom_rag_prompt
-                | llm
-                | StrOutputParser()
-            )
-            full = st.write_stream(rag_chain.stream(text))
-            with st.expander("Código markdown"):
-                st.code(full, language="markdown")
-
+t20 = init_data()
 
 st.title("AjudanTe20 (AT20)")
 st.write("Um assistente para mestres do sistema Tormenta20 para ajudar na criação de ganchos e NPCs. O assistente AT20 também consegue tirar dúvidas sobre regras do sistema.")
@@ -159,7 +52,7 @@ with st.form('ask_llm'):
             "[O que é isso?](https://github.com/Tsukalos/T20-Ajudante?#par%C3%A2metros)")
     submitted = st.form_submit_button('Enviar')
     if submitted and text != '':
-        generate_response(text)
+        t20.generate_response(text, st)
 
 
 footer = """<style>
